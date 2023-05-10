ceebe043d0a296644e9f404b1afb79f7eccc3990
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2023-05-10 21:43:39 +0530

    Initial Commit

--- models/User.js
+++ models/User.js
@@ -20,24 +20,10 @@ const UserSchema = new mongoose.Schema({
 })
 
 // Mongoose middleware for Hashing Password using "bcryptjs"
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
-  //next()
-
-  // In mongoose 5.x, instead of calling next() manually, you can use a function that returns a promise. In particular, you can use async/await.
 })
-
-// Create json web token
-UserSchema.methods.createJWT = function () {
-  return jwt.sign(
-    { userId: this._id, name: this.name },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_LIFETIME
-    }
-  )
-}
 
 // Compare password
 UserSchema.methods.comparePassword = async function (sentPassword) {
