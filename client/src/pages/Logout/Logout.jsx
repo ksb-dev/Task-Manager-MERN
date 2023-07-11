9546c497401d216f0d22e5740d8a0577a3602685
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2023-07-11 13:10:40 +0530

    Initial Commit

--- client/src/pages/Logout/Logout.jsx
+++ client/src/pages/Logout/Logout.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 // react-icons
 import { BiArrowBack, BiLogOutCircle } from 'react-icons/bi'
 import { RiDeleteBin6Line } from 'react-icons/ri'
-import { MdOutlineCancel } from 'react-icons/md'
 
 // tanstack-query
 import { useMutation } from '@tanstack/react-query'
@@ -67,9 +66,8 @@ const Logout = () => {
   return (
     <div className={'logout-page ' + (mode ? 'darkColor' : 'lightColor')}>
       <PrimaryBtn path={'/'} icon={<BiArrowBack />} text={'Back To Home'} />
-      {/* <p>Logout / Delete</p> */}
 
-      <div className={'logout-options '}>
+      <div className='logout-options'>
         <div className='logoutBtn-1' onClick={handleLogout}>
           <span className='logout-icon-1'>
             <BiLogOutCircle />
@@ -91,15 +89,9 @@ const Logout = () => {
 
           <div className='options'>
             <p className='cancel-btn' onClick={() => setShow(!show)}>
-              {/* <span>
-                <MdOutlineCancel />
-              </span> */}
               Cancel
             </p>
             <p className='delete-btn' onClick={handleDeleteAccount}>
-              {/* <span>
-                <RiDeleteBin6Line />
-              </span> */}
               Delete
             </p>
           </div>
