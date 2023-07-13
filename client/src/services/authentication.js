99401b79009dd37c33e6fae86e04b1395591cea8
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2023-07-13 15:14:17 +0530

    Initial Commit

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 //const auth_url = '/url/api/v1/tasks/auth'
 //const auth_url = 'http://localhost:5000/api/v1/tasks/auth'
 const auth_url = '/api/v1/tasks/auth'
+//const delete_account = '/url/api/v1/tasks/delete/account'
+const delete_account = '/api/v1/tasks/delete/account'
 
 export const signup = async ({ name, email, password, image }) => {
   const response = await axios.post(auth_url + '/register', {
@@ -24,7 +26,7 @@ export const login = async ({ email, password }) => {
 }
 
 export const deleteAccount = async token => {
-  const response = await axios.get('/url/api/v1/tasks/delete/account', {
+  const response = await axios.get(delete_account, {
     headers: {
       Authorization: `Bearer ${token}`
     }
